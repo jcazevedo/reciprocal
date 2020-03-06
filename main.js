5e2391a1e53af64be8f57d8fb0964b5002cebc26
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-06 19:48:41 +0000

    Add initial implementation

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-f = (a, b) => a + b
+f = (alphabet, key, text) => {
+  let tabula = [];
+  for (let i = 0; i < alphabet.length; ++i) {
+    tabula[i] = [];
+    for (let j = 0; j < alphabet.length; ++j) {
+      tabula[i][j] = alphabet[(j + i) % alphabet.length];
+    }
+  }
+  let res = "";
+  let skip = 0;
+  for (let i = 0; i < text.length; ++i) {
+    let resC = -1;
+    for (let j = 0; j < tabula[0].length; ++j) {
+      if (tabula[0][j] == text[i]) {
+        resC = j;
+        break;
+      }
+    }
+    if (resC == -1) {
+      skip++;
+      continue;
+    }
+    let resR = -1;
+    for (let j = 0; j < tabula.length; ++j) {
+      if (tabula[j][resC] == key[(i - skip) % key.length]) {
+        resR = j;
+        break;
+      }
+    }
+    if (resR == -1) {
+      res += text[i];
+    } else {
+      res += tabula[resR][0];
+    }
+  }
+  return res;
+}
--- reciprocal_test.js
+++ reciprocal_test.js
@@ -11,10 +11,13 @@ let nTests = 200;
 let maxA = 200;
 let maxB = 200;
 
-for (let t = 0; t < nTests; t++) {
-  let a = Math.floor(Math.random() * maxA) + 1;
-  let b = Math.floor(Math.random() * maxB) + 1;
-  let resCorrect = correctF(a, b);
-  let res = newF(a, b);
-  assert.equal(resCorrect, res);
-}
+let result = newF("SECRT", "SECRET", "SECRAETAB");
+console.log(result);
+
+// for (let t = 0; t < nTests; t++) {
+//   let a = Math.floor(Math.random() * maxA) + 1;
+//   let b = Math.floor(Math.random() * maxB) + 1;
+//   let resCorrect = correctF(a, b);
+//   let res = newF(a, b);
+//   assert.equal(resCorrect, res);
+// }
