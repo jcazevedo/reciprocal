70febf6c4e197485bb4feef404df3c954bfd8a92
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-07 19:45:29 +0000

    Trying 98

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-f = (alphabet, key, text, i=0, a2=alphabet+alphabet) =>
+f = (alphabet, key, text, i=0) =>
   [...text].map(ch => 
-                (~(c = alphabet.indexOf(ch))) ? (a2[a2.indexOf(key[i++ % key.length], c) - c]||ch)  : ""
+                (~(c = alphabet.indexOf(ch))) ? (alphabet[(alphabet+alphabet).indexOf(key[i++ % key.length], c) - c]||ch)  : ""
                ).join``;
