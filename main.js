e9ecf0c5b40844bd0bca38027a8596cded8919cb
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-07 14:09:41 +0000

    Trying 100

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-f = (alphabet, key, text, i=0, a2=alphabet+alphabet) =>
-  [...text].map(ch => 
-                (~(c = alphabet.search(ch))) ? (a2[a2.indexOf(key[i++ % key.length], c) - c]||ch)  : ""
+f = (alphabet, key, [...text], i=0, a2=alphabet+alphabet) =>
+  text.map(ch => 
+                (~(c = alphabet.indexOf(ch))) ? (a2[a2.indexOf(key[i++ % key.length], c) - c]||ch)  : ""
                ).join``;
