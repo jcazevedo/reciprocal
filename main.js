276f46847ec952307532e37c42870c123a820d09
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-07 04:23:23 +0000

    Trying 116

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 f = (alphabet, key, text, i=0, l = alphabet.length,res="",a2=alphabet+alphabet) =>
   [...text].map(ch => {
     if (~(c = alphabet.search(ch))) {
-      r=a2.indexOf(key[i % key.length], c);
-      ++i;
+      r=a2.indexOf(key[i++ % key.length], c);
       res+=(r == -1 ? ch : a2[r - c]);
     }
   })&&res;
