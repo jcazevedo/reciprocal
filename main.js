38fde7f5c8bbb9932fa42dec762a1d9a22e479b8
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-07 04:26:03 +0000

    Trying 113

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 f = (alphabet, key, text, i=0, l = alphabet.length,res="",a2=alphabet+alphabet) =>
   [...text].map(ch => {
-    if (~(c = alphabet.search(ch))) {
-      r=a2.indexOf(key[i++ % key.length], c);
-      res+=(r == -1 ? ch : a2[r - c]);
-    }
+    res+=(~(c = alphabet.search(ch))) ? ((~(r=a2.indexOf(key[i++ % key.length], c))) ? a2[r - c] : ch)  : ""
   })&&res;
