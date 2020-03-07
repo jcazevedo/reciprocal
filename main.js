5ae5250d8ff15afcd337631105cb9debb79e8d06
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-07 04:32:27 +0000

    Trying 109

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-f = (alphabet, key, text, i=0, l = alphabet.length,res="",a2=alphabet+alphabet) =>
-  [...text].map(ch => {
-    res+=(~(c = alphabet.search(ch))) ? ((~(r=a2.indexOf(key[i++ % key.length], c))) ? a2[r - c] : ch)  : ""
-  })&&res;
+f = (alphabet, key, text, i=0, l = alphabet.length,a2=alphabet+alphabet) =>
+  [...text].map(ch => 
+    (~(c = alphabet.search(ch))) ? ((~(r=a2.indexOf(key[i++ % key.length], c))) ? a2[r - c] : ch)  : ""
+               ).join("");
