7bc8feb74871b4a8b6e4bebf18a1326a427e83b6
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-07 04:22:46 +0000

    Trying 118

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ f = (alphabet, key, text, i=0, l = alphabet.length,res="",a2=alphabet+alphabet) 
     if (~(c = alphabet.search(ch))) {
       r=a2.indexOf(key[i % key.length], c);
       ++i;
-      res+=(r == -1 ? ch : alphabet[(r - c) % l]);
+      res+=(r == -1 ? ch : a2[r - c]);
     }
   })&&res;
