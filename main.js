14986f7b154bb0552d3a3aad04969302936dc1f4
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-03-11 23:34:43 +0000

    Trying 93

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-f=(a,k,t,i=j="")=>t.replace(/./g,h=>~(c=a.indexOf(h))?a[(a+a).indexOf((j+=k)[i++],c)-c]||h:"")
+f=(a,k,t,i=j=1)=>t.replace(/./g,h=>~(c=a.indexOf(h))?a[(a+a).indexOf((j+=k)[i++],c)-c]||h:"")
